ae66fb2157c1c8bbd1222985a0a01db41b4ec496
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-05-31 18:34:12 +0100

    fix: activate navbar

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
 import Feed from './pages/Feed';
 
-
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
-    <LandingPage/>
+      <LandingPage />
       {/* <Feed /> */}
     </>
   );
--- apps/client/src/components/SectionTitle.tsx
+++ apps/client/src/components/SectionTitle.tsx
@@ -1,23 +1,31 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading } from '@chakra-ui/react';
 
-interface SectionTitleProps{
-    title: string;
-    subTitle: string;
-
+interface SectionTitleProps {
+  title: string;
+  subTitle: string;
 }
-export default function SectionTitle(props: SectionTitleProps){
-
-
-    return(
-        <>
-        <Heading as="h1" size="lg" marginBottom="4" color="blue.800" marginTop="1rem"
-//  bgGradient='linear(to-r, blue.200, blue.700)'
- >
-            {props.title}
-        </Heading>
-        <Heading as="h3" size="md" marginBottom="4" color="blue.600" marginTop="1rem">
-                {props.subTitle}
-        </Heading>
-            </>
-    )
+export default function SectionTitle(props: SectionTitleProps) {
+  return (
+    <>
+      <Heading
+        as='h1'
+        size='lg'
+        marginBottom='4'
+        color='blue.800'
+        marginTop='1rem'
+        //  bgGradient='linear(to-r, blue.200, blue.700)'
+      >
+        {props.title}
+      </Heading>
+      <Heading
+        as='h3'
+        size='md'
+        marginBottom='4'
+        color='blue.600'
+        marginTop='1rem'
+      >
+        {props.subTitle}
+      </Heading>
+    </>
+  );
 }
