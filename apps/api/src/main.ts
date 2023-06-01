acad8dd1ff5a189d14c1a84f8f6f8e4549b1e9ab
louay <louay.badri@insat.ucar.tn>
2023-06-01 14:34:47 +0100

    doc

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,19 @@ import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import * as express from "express";
 import * as path from "path";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 async function bootstrap() {
   dotenv.config();
   // console.log(process.env.DATABASE)
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const swaggerDocumentOptions = new DocumentBuilder()
+      .setTitle('fruitShop')
+      .setDescription('Fruit Shop Application')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, swaggerDocumentOptions);
+  SwaggerModule.setup('documentation', app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: false, whitelist: true }));
   const staticDir = path.resolve(__dirname, '..', 'uploads');
   app.use('/uploads',express.static(staticDir))
